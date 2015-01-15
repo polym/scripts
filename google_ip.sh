ba173f26803dc1f5bcd7847cbc4a6cccd707dc00
hongbo.mo <hongbo.mo@upai.com>
2015-01-15 11:49:01 +0800

    add upload

--- google_ip.sh
+++ google_ip.sh
@@ -1,11 +1,13 @@
 curl -v "http://www.ipip.net/ping.php?a=send&host=google.com&area=other" -m 60 > /tmp/google_ip.raw
 cat /tmp/google_ip.raw | egrep -o 'ip":"([0-9]{1,3}\.){3}[0-9]{1,3}' | awk -F '"' '{print $3}'| sort -u > /tmp/google_ip
 > /tmp/good
-for ip in `cat /tmp/google_ip`:
+for ip in `cat /tmp/google_ip`
 do
-   curl --connect-timeout 5 $ip 2>&1 > /dev/null
+   curl --connect-timeout 5 -m 10 $ip 2>&1 > /dev/null
    if [ $? -ne 28 ]
    then
        echo $ip >> /tmp/good
    fi
 done
+
+curl -v -T /tmp/good http://v0.api.upyun.com/google-ip/good -u "myworker:tyghbnTYGHBN"
